28ada8d9dea42a0e5e6a02a622dcebee9d211aed
Jens Adria <jensadria@gmail.com>
2022-06-16 19:41:33 +0800

    render header fix

--- client/components/kids/kidTasks.js
+++ client/components/kids/kidTasks.js
@@ -1,37 +1,32 @@
-import { createAnElement } from '../../utils/elementCreator.js';
-
+import { createAnElement } from "../../utils/elementCreator.js";
 
 export function kidTasks() {
-    return axios.get('/api/kids/tasks').then((response) => {
-        const kidTasks = response.data
-        const tasksBox = createAnElement('ul', {
-            id: 'kidTasks'
-        })
-        const taskHeader = createAnElement('h1', {
-            textContent: 'Tasks List'
-        })
-        tasksBox.appendChild(taskHeader)
+  return axios.get("/api/kids/tasks").then((response) => {
+    const kidTasks = response.data;
+    const tasksBox = createAnElement("ul", {
+      id: "kidTasks",
+    });
+    const taskHeader = createAnElement("h1", {
+      textContent: "Tasks List",
+    });
+    tasksBox.appendChild(taskHeader);
+    for (const task of kidTasks) {
+      if ((task.status = "approved" && task.cents)) {
+        const newTask = createAnElement("div", {
+          id: "eachTask",
+          textContent: `${task.description}  $${task.cents * 0.1}`,
+        });
 
-        for (const task of kidTasks) {
-            if (task.status = 'approved' && task.cents) {
-            const newTask = createAnElement('div', {
-                id: 'eachTask',
-                textContent: `${task.description}  $${task.cents * 0.1}`
-            })
-            
-            tasksBox.appendChild(newTask)
-            return tasksBox
-        }
-        else if (task.status = 'approved' && task.points){
-            const newTask = createAnElement('div', {
-                id: 'eachTask',
-                textContent: `${task.description}  ${task.points} points`
-            })
-            
-            tasksBox.appendChild(newTask)
-            return tasksBox
-        }
+        tasksBox.appendChild(newTask);
+      } else if ((task.status = "approved" && task.points)) {
+        const newTask = createAnElement("div", {
+          id: "eachTask",
+          textContent: `${task.description}  ${task.points} points`,
+        });
 
-        }
-    })
+        tasksBox.appendChild(newTask);
+      }
+    }
+    return tasksBox;
+  });
 }
