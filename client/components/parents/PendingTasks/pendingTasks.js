c36043ffeec798afe1ae0f547b8e7795f7253df4
Rinasham <rinashamau@gmail.com>
2022-06-20 02:19:28 +1000

    [add] approve/ reject buttons

--- client/components/parents/PendingTasks/pendingTasks.js
+++ client/components/parents/PendingTasks/pendingTasks.js
@@ -1,6 +1,7 @@
 import { createAnElement } from "../../../utils/elementCreator.js";
 import { store } from "../../../utils/store.js";
 import { createTitle } from "../../title.js";
+import { approverejectConfirm } from "../Form/approveRejectConfirm.js";
 
 export function approvePendingTasks() {
   console.log("ok");
@@ -32,6 +33,164 @@ export function approvePendingTasks() {
       pendingTasksWrapper.appendChild(emptyMessage);
       return pendingTasksWrapper;
     }
-    return res.data;
+
+    // this object holds 'kids name : [{ task1 }, { task2 }, ...]'
+    const pendingTasksObj = {};
+
+    // loop over all tasks list array and create task elements one by one
+    // each element contains "description" + "points/cents"
+    pendingTasksList.forEach((task) => {
+      if (task.name in pendingTasksObj) {
+        const requestedTask = {
+          task_id: task.t_id,
+          description: task.description,
+          cents: task.cents,
+          points: task.points,
+        };
+        pendingTasksObj[task.name].push(requestedTask);
+      } else {
+        pendingTasksObj[task.name] = [
+          {
+            task_id: task.t_id,
+            description: task.description,
+            cents: task.cents,
+            points: task.points,
+          },
+        ];
+      }
+    });
+
+    // Loop over the "kids : [tasks]" object
+    //create a tasks container for each kid (and add tasks elements later)
+    Object.keys(pendingTasksObj).forEach((kidName) => {
+      // this container is to wrap task elements later
+      const tasksListContainer = createAnElement("div", {
+        className: "container",
+      });
+
+      // to show each kid's name above tasks container
+      const name = createAnElement("p", {
+        className: "kids-name-for-tasklist",
+        textContent: kidName,
+      });
+      pendingTasksWrapper.appendChild(name);
+      // create tasks elements for all the kids
+      pendingTasksObj[kidName].forEach((task) => {
+        // container > taskElement > "task = taslElement + buttons container: (ApproveBtn + rejectBtn)"
+        const taskWrapper = createAnElement("div", {
+          className: "taskItem",
+        });
+
+        // taskElement contains description, points/cents
+        const taskElement = createTaskElement(task);
+        taskWrapper.appendChild(taskElement);
+
+        const btnsContainer = createAnElement("div", {
+          className: "approve-reject-wrapper",
+        });
+
+        let rewardType = "";
+        let approveBtn;
+
+        if (task.cents) {
+          approveBtn = createAnElement("button", {
+            textContent: "Approve",
+            className: "approve-reject",
+            value: task.task_id, // task id
+            id: `${task.description}:${task.cents}`,
+          });
+          rewardType = "cents";
+          btnsContainer.appendChild(approveBtn);
+        } else {
+          approveBtn = createAnElement("button", {
+            textContent: "Approve",
+            className: "approve-reject",
+            value: task.task_id, // task id
+            id: `${task.description}:${task.points}`,
+          });
+          rewardType = "points";
+          btnsContainer.appendChild(approveBtn);
+        }
+
+        const rejectBtn = createAnElement("button", {
+          textContent: "Reject",
+          className: "approve-reject",
+          value: task.task_id, // task id
+          id: task.description,
+        });
+        btnsContainer.appendChild(rejectBtn);
+        taskWrapper.appendChild(btnsContainer);
+
+        tasksListContainer.appendChild(taskWrapper);
+
+        //========== event listener for approve/reject buttons ===========
+        approveBtn.addEventListener("click", (e) => {
+          const targetedTask = {
+            task_id: e.target.value,
+            task_description: e.target.id,
+          };
+          if (rewardType === "cents") {
+            approverejectConfirm("approve", targetedTask, "cents", "pending");
+          } else {
+            approverejectConfirm("approve", targetedTask, "points", "pending");
+          }
+        });
+
+        rejectBtn.addEventListener("click", (e) => {
+          const targetedTask = {
+            task_id: e.target.value,
+            task_description: e.target.id,
+          };
+          approverejectConfirm("reject", targetedTask);
+        });
+      });
+      // ============================================================
+
+      // append everything in the wrapper and return
+      pendingTasksWrapper.appendChild(tasksListContainer);
+    });
+
+    return pendingTasksWrapper;
   });
+}
+
+// returns "description + points" or "description + cents"
+function createTaskElement(task) {
+  if (task.cents) {
+    const description = createAnElement("p", {
+      className: "taskDescription",
+      textContent: task.description,
+    });
+    const money = createAnElement("p", {
+      className: "money",
+      textContent: `$ ${task.cents / 100}`,
+    });
+    const taskElement = createAnElement(
+      "div",
+      {
+        className: "task item",
+      },
+      [description, money]
+    );
+    return taskElement;
+  }
+
+  if (task.points) {
+    const description = createAnElement("p", {
+      className: "taskDescription",
+      textContent: task.description,
+    });
+    const points = createAnElement("p", {
+      className: "points",
+      textContent: `${task.points} pts`,
+    });
+    const taskElement = createAnElement(
+      "div",
+      {
+        className: "task item",
+      },
+      [description, points]
+    );
+    return taskElement;
+  }
 }
--- client/components/parents/api/postApproveReject.js
+++ client/components/parents/api/postApproveReject.js
@@ -2,25 +2,27 @@ import { renderPage } from "../../../index.js";
 
 // appvoroOrReject : String
 // if approve, send a request to delete the targeted task
-export function postApproveReject(form, appvoroOrReject, data) {
+export function postApproveReject(form, appvoroOrReject, data, requestType) {
   const taskId = form.task_id.value;
   const jsonData = data;
   const errorMessage = document.getElementById("error-message");
 
-  if (appvoroOrReject === "approve") {
-    // send a delete request to the server
-    axios
-      .patch(`/api/parents/task/${taskId}`, jsonData)
-      .then(() => {
-        renderPage();
-      })
-      .catch((err) => {
-        console.log(err);
-        errorMessage.textContent =
-          "There was a server error. Please try again.";
-        errorMessage.style.display = "block";
-      });
-  } else {
-    console.log("reject confirmed");
-  }
+  console.log(requestType);
+
+  // if (appvoroOrReject === "approve") {
+  //   // send a delete request to the server
+  //   axios
+  //     .patch(`/api/parents/task/${taskId}`, jsonData)
+  //     .then(() => {
+  //       renderPage();
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       errorMessage.textContent =
+  //         "There was a server error. Please try again.";
+  //       errorMessage.style.display = "block";
+  //     });
+  // } else {
+  //   console.log("reject confirmed");
+  // }
 }
