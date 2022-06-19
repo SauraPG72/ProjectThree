48377b3779cb6812e331982121daa5d167d4ff43
Jens Adria <jensadria@gmail.com>
2022-06-19 16:46:20 +0800

    Added add-kid-prompt components, added assets and illustrations

--- client/components/main/addKidPrompt.js
+++ client/components/main/addKidPrompt.js
@@ -1,26 +1,45 @@
 import { createAnElement } from "../../utils/elementCreator.js";
 
 export function renderAddKidPrompt() {
-  const addKidPromptHeader = createAnElement("h1", {
-    id: "splash-header",
-    textContent: "Set Up an account for your kids!",
+  const addKidPromptIllustration = createAnElement("img", {
+    id: "add-kid-prompt-image",
+    src: "./assets/illustrations/addkid.svg",
   });
 
+  const addKidPromptHeader = createAnElement("h1", {
+    id: "add-kid-prompt-header",
+    textContent: "You are one step closer to giving your kids financial freedom!",
+  });
+  const addKidPromptParagraph = createAnElement("p", {
+    id: "add-kid-prompt-paragraph",
+    textContent: "Create an account for your kids to get things started",
+  });
   const addKidButton = createAnElement("button", {
     id: "add-kid-btn",
     textContent: "Set Up An Account Now",
   });
 
+  const addKidPromptHeaderContainer = createAnElement(
+    "div",
+    {
+      id: "add-kid-prompt-header-container",
+    },
+    [addKidPromptHeader, addKidPromptParagraph, addKidButton]
+  );
+
+  // container for the add kids prompt
   const addKidCallToActionContainer = createAnElement("div", { id: "add-kid-prompt-container" }, [
-    addKidPromptHeader,
+    addKidPromptHeaderContainer,
+    addKidPromptIllustration,
   ]);
 
+  //  container for the whole main section
   const mainContainer = createAnElement(
     "main",
     {
       className: "main-container",
     },
-    [addKidCallToActionContainer, addKidButton]
+    [addKidCallToActionContainer]
   );
 
   app.append(mainContainer);
