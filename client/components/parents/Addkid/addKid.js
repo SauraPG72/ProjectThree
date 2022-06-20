c5df98da45af1b7d2e01b6c7c83f54654229aa5e
Jens Adria <jensadria@gmail.com>
2022-06-20 22:12:10 +0800

    Revert some changes

--- client/components/parents/Addkid/addKid.js
+++ client/components/parents/Addkid/addKid.js
@@ -10,7 +10,7 @@ export function addKidPage() {
 
   // wrapper > form > inputs/ button
   const placeHolders = [
-    "Username",
+    "Name",
     { name: "Password", type: "password" },
     { name: "Confirm Password", type: "password" },
     "Starting Reward Points",
