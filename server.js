e81453901a05109173a19d3ae0ff4abfac54a077
Rinasham <rinashamau@gmail.com>
2022-06-23 00:26:26 +1000

    [fix] setting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const db = require("./db/db");
 const expressSession = require("express-session");
 const pgSession = require("connect-pg-simple")(expressSession);
